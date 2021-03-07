21d64b9ecce38bc2753d4b3c0644897bf95d029a
blog20170623 <94119316@qq.com>
2021-03-07 15:06:43 +0800

    Site updated: 2021-03-07 15:06:35

---
+++ shares/js/custom.js
@@ -0,0 +1,2 @@
+$(window).load(function(){$('.preloader').fadeOut("slow");});$(function(){$('.navbar-collapse a').click(function(){$(".navbar-collapse").collapse('hide');});$('#portfolio a').nivoLightbox({effect:'fadeScale',});wow=new WOW({mobile:false});wow.init();loadGoogleMap();function initParallax(){$('#home').parallax("100%",0.3);$('#team').parallax("100%",0.3);$('#contact').parallax("100%",0.1);}
+initParallax();});
